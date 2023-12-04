824842ac363d8449736e88017a3c6cb6e4fee1fe
Marek Skrzypiński <skrzypinski009@gmail.com>
2023-12-04 16:53:15 +0100

    some_fixes

--- src/views/dvd_search_view.py
+++ src/views/dvd_search_view.py
@@ -1,9 +1,11 @@
 from tkinter import ttk
+import tkinter as tk
 
 class DVDSearchView(ttk.Frame):
     def __init__(self, parent, controler):
-        super().__init__(parent)
-        self.pack()
-
-        label = ttk.Label(self, text="DVDSearchView")
-        label.pack()
+        super().__init__(parent, width=parent.winfo_reqwidth())
+        self.configure(style='View.TFrame')
+        for i in range(0,40):
+            label = ttk.Label(self, text="DVDSearchView")
+            label.configure(style='View.TLabel')
+            label.pack()
