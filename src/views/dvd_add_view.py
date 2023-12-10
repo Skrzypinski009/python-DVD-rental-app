5025be589c89dfd771469d9a46f376e83627afb1
Marek Skrzypiński <skrzypinski009@gmail.com>
2023-12-10 16:16:51 +0100

    changing Entry to Text widget for Description, and small fixes

--- src/views/dvd_add_view.py
+++ src/views/dvd_add_view.py
@@ -6,21 +6,21 @@ class DVDAddView(ttk.Frame):
         super().__init__(parent, width=parent.winfo_reqwidth())
         container = ttk.Frame(self)
         container.pack(fill='x', expand=True, padx=(50,0), pady=(50,0))
-        ttk.Label(container, text="Dodawanie nowej płyty DVD", font=('Arial', 22)).pack(anchor='w', pady=(8,20))
+        ttk.Label(container, text="Dodawanie nowej płyty DVD", font=('Arial', 22)).pack(pady=(8,20))
         # Pole do wpisania nazwy
         ttk.Label(container, text='Nazwa', font=('Arial', 16)).pack(anchor='w')
-        ttk.Entry(container).pack(anchor='w', pady=(8,20))
+        ttk.Entry(container, width=30).pack(anchor='w', pady=(8,20))
         # Pole do wpisania opisu
         ttk.Label(container, text='Opis', font=('Arial', 16)).pack(anchor='w')
-        ttk.Entry(container).pack(anchor='w', pady=(8,20))
+        tk.Text(container, width=50, height=6).pack(anchor='w', pady=(8,20))
         # Pole do wpisania roku wydania
         ttk.Label(container, text='Rok wydania', font=('Arial', 16)).pack(anchor='w')
-        ttk.Entry(container).pack(anchor='w', pady=(8,20))
+        ttk.Entry(container, width=30).pack(anchor='w', pady=(8,20))
         # Pole do wpisania kategorii
         ttk.Label(container, text='Kategorie (wymień po przecinku)', font=('Arial', 16)).pack(anchor='w')
-        ttk.Entry(container).pack(anchor='w', pady=(8,20))
+        ttk.Entry(container, width=30).pack(anchor='w', pady=(8,20))
         # Pole do wpisania kodów fizycznych kopii
         ttk.Label(container, text='Kody fizycznych kopii (wymień po przecinku)', font=('Arial', 16)).pack(anchor='w')
-        ttk.Entry(container).pack(anchor='w', pady=(8,20))
+        ttk.Entry(container, width=30).pack(anchor='w', pady=(8,20))
         # Przycisk do stworzenia zapisu o płycie DVD
-        ttk.Button(container, text='Stwórz').pack(anchor='w', padx=50, pady=10)
+        ttk.Button(container, width=30, text='Stwórz').pack(anchor='w', padx=50, pady=10)
