5fcf1fa0885d3c2f9ca345efe33dfaa5165b9406
Marek Skrzypiński <skrzypinski009@gmail.com>
2023-12-29 20:36:52 +0100

    adding methods for dvd category relation

--- src/database/database_manager.py
+++ src/database/database_manager.py
@@ -13,7 +13,7 @@ class DatabaseManager:
 
     def all_tables_create(self, cursor, connection):
         self.dvd_table_create(cursor, connection)
-
+        self.dvd_category_relation_table_create(cursor, connection)
         # kolejne tworzenie tabel
 
     def dvd_table_create(self, cursor, connection):
@@ -23,19 +23,29 @@ class DatabaseManager:
                        date TIMESTAMP)''')
         connection.commit()
 
+    def dvd_category_relation_table_create(self, cursor, connection):
+        cursor.execute('''CREATE TABLE IF NOT EXIST dvd_category_relation
+                       (id INTEGER primary key autoincrement
+                       dvd_id INTEGER NOT NULL,
+                       FOREIGN KEY (dvd_id))''')
+        connection.commit()
+
+
     def all_tables_delete(self, cursor, connection):
         self.dvd_table_delete(cursor, connection)
+        self.dvd_category_relation_table_delete(cursor, connection)
         #kolejne usuwanie tabel
 
     def dvd_table_delete(self, cursor, connection):
         cursor.execute('''DROP TABLE IF EXIST dvd''')
         connection.commit()
 
-
     def category_table_create(self, cursor, connection):
             cursor.execute('''CREATE TABLE IF NOT EXIST category
                         (id INTEGER primary key autoincrement
                         name TEXT)''')
-            
-            
             connection.commit()
+
+    def dvd_category_relation_table_delete(self, cursor, connection):
+        cursor.execute('''DROP TABLE IF EXIST dvd_category_relation''')
+        connection.commit()
