677339c509b231ec222690e1c8767edcbf51db2a
Marek Skrzypiński <skrzypinski009@gmail.com>
2023-12-29 20:20:27 +0100

    adding database_manager class

---
+++ src/database/database_manager.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+class DatabaseManager:
+    def __init__(self):
+        self.check_database()
+
+    def check_database(self):
+        conn = sqlite3.connect("data/database.db")
+        cur = conn.cursor()
+        self.all_tables_create()
+        cur.close()
+        conn.close()
+
+    def all_tables_create(self, cursor, connection):
+        self.dvd_table_create(cursor, connection)
+
+        # kolejne tworzenie tabel
+
+    def dvd_table_create(self, cursor, connection):
+        cursor.execute('''CREATE TABLE IF NOT EXIST dvd
+                       (id INTEGER primary key autoincrement
+                       name TEXT
+                       date TIMESTAMP)''')
+        connection.commit()
+
+    def all_tables_delete(self, cursor, connection):
+        self.dvd_table_delete(cursor, connection)
+        #kolejne usuwanie tabel
+
+    def dvd_table_delete(self, cursor, connection):
+        cursor.execute('''DROP TABLE IF EXIST dvd''')
+        connection.commit()
